5fc500f30497ad0b7f5a7f3673aa3a322cd032c5
Veerdhaval Shinde <134299614+veerDS@users.noreply.github.com>
2026-04-07 15:50:53 +0530

    Update GlobalDriver.java

--- src/main/java/com/webDriver/GlobalDriver.java
+++ src/main/java/com/webDriver/GlobalDriver.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import com.utils.LimitBrowser;
 
 public class GlobalDriver {
